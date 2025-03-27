f7613130418cfc1d4a0824307c32aa85eca1f4c9
Manikanta <manikanta.paidimarri@apoorva.com>
2025-03-27 10:50:38 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ANGHASSS';
+  @ViewChild('passwordInput') passwordInput!: ElementRef;
+  @ViewChild('togglePassword') togglePassword!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  togglePasswordVisibility() {
+    const inputType = this.passwordInput.nativeElement.type;
+    this.passwordInput.nativeElement.type = inputType === 'password' ? 'text' : 'password';
+
+    // Change icon
+    const newIcon = inputType === 'password' ? 'bi bi-eye' : 'bi bi-eye-slash';
+    this.renderer.setProperty(this.togglePassword.nativeElement, 'innerHTML', `<i class="${newIcon}"></i>`);
+  }
 }
